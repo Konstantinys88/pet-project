86bfe362e4c9fcfbf00f10196a99c48a4f852de0
Maschkov Konstantin <maschkovc@yandex.ru>
2023-12-18 18:29:47 +0300

    add weatherApi

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -1,7 +1,15 @@
+"use client"
 import './weather.scss';
+import weatherApi from '@/services/weatherApi';
 
 
 export default function Weather() {
+
+    const { getResource } = weatherApi();
+    // getResource();
+    getResource().then(res => console.log(res));
+    // console.log(getResource())
+
     return (
         <div className='weather'>
             Погода
