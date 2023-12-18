242f98a07428a0f09125a79e6e167637ca1a9678
Maschkov Konstantin <maschkovc@yandex.ru>
2023-12-18 14:25:34 +0300

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-
+import './home.scss';
+import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<div>
-			main page
+		<div className="home">
+			<div className="home__wrapper">
+				<div className='home__description'>
+					<p className='home__description_text'>Далеко-далеко за словесными горами в стране гласных и согласных, живут рыбные тексты. Подзаголовок назад маленькая запятой свое парадигматическая своих возвращайся от всех семь, проектах переписали, точках родного пор предложения повстречался имеет, журчит большой.</p>
+				</div>
+				<div className='home__image-block'>
+					<Image
+						className='home__image-block_img'
+						src="/tororo.jpg"
+						width={400}
+						height={400}
+						alt="Picture of the author" />
+				</div>
+			</div>
+
 		</div>
 	)
 }
